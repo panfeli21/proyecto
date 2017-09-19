4d5f81a772724fcf83d23daf254970c344122d61
panfeli21 <30870193+panfeli21@users.noreply.github.com>
2017-09-18 19:12:32 -0500

    lista simple

--- src (1)/src/estructurasdedatos/cap2/listas/ListaSimple.java
+++ src (1)/src/estructurasdedatos/cap2/listas/ListaSimple.java
@@ -5,10 +5,107 @@
  */
 package estructurasdedatos.cap2.listas;
 
+import estructurasdedatos.utiles.nodos.NodoSimple;
+
 /**
  *
  * @author tusk
  */
 public class ListaSimple {
     
-}
+   
+    
+    private NodoSimple cabeza;
+    private NodoSimple cola;
+    
+  public void CrearLista (){ // aqui creo una lista con cabeza y cola 
+                             //inicializados en null 
+  
+  this.cabeza= null;      
+  this.cola= null;
+  
+  
+  }
+   
+ public void InserDardato (int dato){ // aqui creamos el nuevo nodo y 
+    NodoSimple nnodo= new NodoSimple(); //le asignamos el valor de el dato, luego  
+    nnodo.crearNuevoNodo();   // hacemos un condicional que nos diga si la lista esta vacia o hay que hacer enlaces 
+    nnodo.setdato(dato);
+    
+    if(this.cabeza== null && this.cola== null){
+    this.cabeza= nnodo;
+    this.cola= nnodo;
+    
+    }else {
+    cola.setdato(dato);
+    this.cola=nnodo;
+    
+    }
+    
+ } 
+  public void listar() {
+        // verificiamos que la lista no este vacia 
+        if (!(this.cabeza == null && this.cola == null)) {
+            // creamos el nodo que nos va a recorres la lista nodo 
+            // por nodo imprimedo el valor que hay en cada nodo 
+            NodoSimple aux = cabeza;
+            // ciclo por el cual el nodo recorre la lista hasta que la cola tenga el valor de null
+            int i = 0;
+            while (aux != null) {
+                System.out.println("el dato :"  + "[" + aux.getdato() + "]" + " " + "esta en la posicion"
+                        + "(" + i + ")" + "de la lista");
+
+                aux = aux.getsiguiente();
+                i++;
+            }
+        }
+    
+        
+
+   
+        
+    }
+  
+  
+  public int borrar(int dato){
+      if (dato==cabeza.getdato() ) {
+          this.cabeza= cabeza.getsiguiente();
+      }else {
+          
+      NodoSimple aux = cabeza;
+      
+      while( aux.getsiguiente().getdato() != dato){
+      
+      aux= aux.getsiguiente();
+      
+      }
+     NodoSimple auxsig= aux.getsiguiente().getsiguiente();
+  aux.setsiguiente(auxsig);
+  
+  
+  }
+  
+       
+  return 0;
+  
+  }  
+  
+ public void ActualizarDato(int dato ){
+ 
+ if (dato==cabeza.getdato() ) {
+          this.cabeza= cabeza.getsiguiente();
+      }else {
+          
+      NodoSimple aux = cabeza;
+      
+      while( aux.getsiguiente().getdato() != dato){
+      
+      aux= aux.getsiguiente();
+      
+      } { }
+ 
+ 
+ } 
+  
+  
+}}
